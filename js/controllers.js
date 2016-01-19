1cc6bb09ee620053670106edb869575ef04d331d
Sharon Clark <sclark@FADX3BBZ1.fanda.ucar.edu>
2016-01-19 16:30:18 -0700

    incl possibility of combining graphs

--- js/controllers.js
+++ js/controllers.js
@@ -1,21 +1,67 @@
 angular.module('co2.controllers', [])
-.controller('homeCtrl', function ($scope,$window){
-    $scope.device_width = $window.innerWidth;
-    $scope.graph_height = $window.innerHeight/3;
-    // ipad pro resolution is 2048x2732, so max height of each, with nav is 682
-})
-.controller('nwrCtrl', function($scope, contentData){
-    $scope.file = "data/nwr.tsv";
-    $scope.title = "Niwot Ridge (NWR)"
-    $scope.bucket_size = "200";
-})
-.controller('splCtrl', function($scope, contentData){
-    $scope.file = "data/spl.tsv";
-    $scope.title = "Storm Peak Laboratory (SPL)"
-    $scope.bucket_size = "20";
-})
-.controller('mloCtrl', function($scope, contentData){
-    $scope.file = "data/mlo.tsv";
-    $scope.title = "Mauna Loa Observatory (MLO)"
-    $scope.bucket_size = "20";
-});
+    .controller('homeCtrl', function ($scope, $window) {
+        $scope.device_width = $window.innerWidth;
+        $scope.device_height = $window.innerHeight;
+        $scope.graph_height = $scope.device_height / 3;
+        // ipad pro resolution is 2048x2732, so max height of each, with nav is 682
+    
+        //TODO: get y_max value from all files
+        $scope.y_min = 300;
+        $scope.y_max = 420;
+
+        // TODO: retrieve min and max date from mlo
+        $scope.min_date = '1958-03-29T12:00:00';
+        $scope.max_date = '2016-01-13T12:00:00';
+
+    })
+    .controller('nwrCtrl', function ($scope, contentData) {
+        $scope.file = "data/nwr.tsv";
+        $scope.title = "Niwot Ridge (NWR)"
+        $scope.bucket_size = "500";
+    })
+    .controller('splCtrl', function ($scope, contentData) {
+        $scope.file = "data/spl.tsv";
+        $scope.title = "Storm Peak Laboratory (SPL)"
+        $scope.bucket_size = "10";
+    })
+    .controller('mloCtrl', function ($scope, contentData) {
+        $scope.file = "data/mlo.tsv";
+        $scope.title = "Mauna Loa Observatory (MLO)"
+        $scope.bucket_size = "20";
+    })
+    .controller('chartCtrl', function ($scope, contentData) {
+        $scope.title = "CO\u2082 (ppm)";
+        // get relevant data values for each plot to graph
+        var data = {};
+        $scope.plotAttr = {
+            "yMin": $scope.yMin,
+            "yMax": $scope.yMax,
+            "minDate": $scope.minDate,
+            "maxDate": $scope.maxDate,
+            "width": $scope.device_width,
+            "height": $scope.device_height,
+            "top": 10,
+            "right": 20,
+            "bottom": 20,
+            "left": 50,
+            "yLabel": $scope.title
+        };
+
+        $scope.mlo = {
+            "file": "data/mlo.tsv",
+            "bucketSize": "20",
+            "color": "blue"
+        };
+
+        $scope.nwr = {
+            "file": "data/nwr.tsv",
+            "bucketSize": "20",
+            "color": "red"
+        }
+        data = {
+            "mlo": $scope.mlo,
+            "nwr": $scope.nwr        
+        }
+
+        $scope.plotValues = data;
+    });
--- js/services.js
+++ js/services.js
@@ -8,28 +8,25 @@ angular.module('co2.services', [])
 
         return contentData;
     }])
-    .factory('d3Func', function () {
-        var d3Func = {};
-        d3Func.drawLineGraph = function (top, right, bottom, left, graphWidth, graphHeight, fileLoc, title, parentDiv, yLabel, bucketSize) {
-
-            if (!bucketSize) {
-                bucketSize = 20;
-            }
-            // functionality based on: http://blog.scottlogic.com/2014/09/19/interactive.html
-
-            // init show 1 year
-            var daysShown = 365;
-
+    .factory('co2Chart', function () {
+        var co2Chart = {};
+        co2Chart.drawLineGraph = function (plotAttr, plotValues) {
             var margin = {
-                    top: top,
-                    right: right,
-                    bottom: bottom,
-                    left: left
+                    top: plotAttr.top,
+                    right: plotAttr.right,
+                    bottom: plotAttr.bottom,
+                    left: plotAttr.left
                 },
-                width = graphWidth - margin.left - margin.right,
-                height = graphHeight - margin.top - margin.bottom;
-
+                width = plotAttr.width - margin.left - margin.right,
+                height = plotAttr.height - margin.top - margin.bottom;
+            var yLabel = plotAttr.yLabel;
             var parseDate = d3.time.format.utc("%Y-%m-%dT%H:%M:%S").parse;
+
+            // retrieve min and max values
+            var minDate = parseDate(plotAttr.minDate);
+            var maxDate = parseDate(plotAttr.maxDate);
+            var yMin = plotAttr.yMin;
+            var yMax = plotAttr.yMax;
 
             // create x and y scales
             var xScale = d3.time.scale()
@@ -43,6 +40,11 @@ angular.module('co2.services', [])
             // axis
             var xAxis = make_x_axis();
             var yAxis = make_y_axis();
+
+
+            // x and y domains
+            yScale.domain([yMin, yMax]).nice();
+            xScale.domain([minDate, maxDate]);
 
             // gridlines
             function make_x_axis() {
@@ -59,16 +61,22 @@ angular.module('co2.services', [])
                     .ticks(5)
             }
 
-            // plotchart
-
-            var svg = d3.select("#" + parentDiv).append("svg")
+            var svg = d3.select("#chart").append("svg")
                 .attr('class', 'plot')
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
                 .append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            var line = d3.svg.line()
+            // line function
+            var line1 = d3.svg.line()
+                .x(function (d) {
+                    return xScale(d.DATE);
+                })
+                .y(function (d) {
+                    return yScale(d.CO2);
+                });
+            var line2 = d3.svg.line()
                 .x(function (d) {
                     return xScale(d.DATE);
                 })
@@ -76,7 +84,166 @@ angular.module('co2.services', [])
                     return yScale(d.CO2);
                 });
 
-            /*
+            // get sampled data
+            for (var key in plotValues) {
+                if (plotValues.hasOwnProperty(key)) {
+                    var fileLoc = plotValues[key].file;
+                    var label = key;
+
+                    d3.tsv(fileLoc, function (error, data) {
+                        if (error) throw error;
+                        var bucketSize = plotValues[label].bucketSize;
+                        var color = plotValues[label].color;
+
+                        // cleanse data to expected form
+                        for (var key in data) {
+                            data[key].DATE = parseDate(data[key].DATE);
+                            data[key].CO2 = parseFloat(data[key].CO2);
+                        }
+
+                        // configure the sampler
+                        var sampler = fc.data.sampler.largestTriangleThreeBucket()
+                            .bucketSize(bucketSize)
+                            .x(function (d) {
+                                return d.DATE;
+                            })
+                            .y(function (d) {
+                                return d.CO2;
+                            });
+
+                        // sample the data
+                        var sampledData = sampler(data);
+
+                        switch (label) {
+                        case "mlo":
+                            svg.append("path")
+                                .datum(sampledData)
+                                .attr("class", "line")
+                                .attr("stroke", color)
+                                .attr("d", line1);
+                            break;
+                        case "nwr":
+                            svg.append("path")
+                                .datum(sampledData)
+                                .attr("class", "line")
+                                .attr("stroke", color)
+                                .attr("d", line2);
+                            break;
+                        }
+
+                    });
+                }
+            }
+
+
+
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text(yLabel);
+
+            svg.append("g")
+                .attr("class", "grid")
+                .attr("transform", "translate(0," + height + ")")
+                .call(make_x_axis()
+                    .tickSize(-height, 0, 0)
+                    .tickFormat("")
+                )
+
+            svg.append("g")
+                .attr("class", "grid")
+                .call(make_y_axis()
+                    .tickSize(-width, 0, 0)
+                    .tickFormat("")
+                )
+
+
+        };
+
+        return co2Chart;
+
+    })
+
+.factory('d3Func', function () {
+    var d3Func = {};
+    d3Func.drawLineGraph = function (top, right, bottom, left, graphWidth, graphHeight, fileLoc, title, parentDiv, yLabel, yMin, yMax, minDate, maxDate, bucketSize) {
+
+        if (!bucketSize) {
+            bucketSize = 20;
+        }
+        // functionality based on: http://blog.scottlogic.com/2014/09/19/interactive.html
+
+        // init show 1 year
+        var daysShown = 365;
+
+        var margin = {
+                top: top,
+                right: right,
+                bottom: bottom,
+                left: left
+            },
+            width = graphWidth - margin.left - margin.right,
+            height = graphHeight - margin.top - margin.bottom;
+
+        var parseDate = d3.time.format.utc("%Y-%m-%dT%H:%M:%S").parse;
+        var minDate = parseDate(minDate);
+        var maxDate = parseDate(maxDate);
+        // create x and y scales
+        var xScale = d3.time.scale()
+            .range([0, width]);
+        var yScale = d3.scale.linear()
+            .range([height, 0]);
+        var chart = fc.chart.cartesian(
+            xScale,
+            yScale);
+
+
+        // gridlines
+        function make_x_axis() {
+            return d3.svg.axis()
+                .scale(xScale)
+                .orient("bottom")
+                .ticks(10)
+        }
+
+        function make_y_axis() {
+            return d3.svg.axis()
+                .scale(yScale)
+                .orient("left")
+                .ticks(5)
+        }
+        // axis
+        var xAxis = make_x_axis();
+        var yAxis = make_y_axis();
+
+        // plotchart
+        var svg = d3.select("#" + parentDiv).append("svg")
+            .attr('class', 'plot')
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var line = d3.svg.line()
+            .x(function (d) {
+                return xScale(d.DATE);
+            })
+            .y(function (d) {
+                return yScale(d.CO2);
+            });
+
+        /*
             // plotarea
             var svgArea = svg.append('g')
                 .attr('clip-path', 'url(#svgAreaClip)');
@@ -89,100 +256,78 @@ angular.module('co2.services', [])
                     height: height
                 });
 */
-            d3.tsv(fileLoc, function (error, data) {
-                if (error) throw error;
+        d3.tsv(fileLoc, function (error, data) {
+            if (error) throw error;
 
-                // cleanse data to expected form
-                for (var key in data) {
-                    data[key].DATE = parseDate(data[key].DATE);
-                    data[key].CO2 = parseFloat(data[key].CO2);
+            // cleanse data to expected form
+            for (var key in data) {
+                data[key].DATE = parseDate(data[key].DATE);
+                data[key].CO2 = parseFloat(data[key].CO2);
+            }
 
-                }
-
-                // configure the sampler
-                var sampler = fc.data.sampler.largestTriangleThreeBucket()
-                    .bucketSize(bucketSize)
-                    .x(function (d) {
-                        return d.DATE;
-                    })
-                    .y(function (d) {
-                        return d.CO2;
-                    });
-
-                // sample the data
-                var sampledData = sampler(data);
-
-                //xScale.domain(fc.util.extent().fields('DATE')(sampledData))
-                //yScale.domain(fc.util.extent().fields('CO2')(sampledData));
-
-                xScale.domain(d3.extent(sampledData, function (d) {
+            // configure the sampler
+            var sampler = fc.data.sampler.largestTriangleThreeBucket()
+                .bucketSize(bucketSize)
+                .x(function (d) {
                     return d.DATE;
-                }));
-                yScale.domain(d3.extent(sampledData, function (d) {
+                })
+                .y(function (d) {
                     return d.CO2;
-                }));
+                });
 
+            // sample the data
+            var sampledData = sampler(data);
+            yScale.domain([yMin, yMax]).nice();
+            xScale.domain([minDate, maxDate]);
 
-                // the sampled data
-                var sampledLine = fc.series.line()
-                    .xScale(xScale)
-                    .yScale(yScale)
-                    .xValue(function (d) {
-                        return d.DATE;
-                    })
-                    .yValue(function (d) {
-                        return d.CO2;
-                    });
+            // the sampled data
+            var sampledLine = fc.series.line()
+                .xScale(xScale)
+                .yScale(yScale)
+                .xValue(function (d) {
+                    return d.DATE;
+                })
+                .yValue(function (d) {
+                    return d.CO2;
+                });
 
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
 
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(yAxis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", ".71em")
+                .style("text-anchor", "end")
+                .text(yLabel);
 
-                // render
-                /* svgArea.append('g')
-                     .attr('class', 'line')
-                     .datum(sampledData)
-                     .call(sampledLine)
-                     .call(gridlines);
-                     
-                     */
+            svg.append("g")
+                .attr("class", "grid")
+                .attr("transform", "translate(0," + height + ")")
+                .call(make_x_axis()
+                    .tickSize(-height, 0, 0)
+                    .tickFormat("")
+                )
 
-             
-                svg.append("g")
-                    .attr("class", "x axis")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(xAxis);
+            svg.append("g")
+                .attr("class", "grid")
+                .call(make_y_axis()
+                    .tickSize(-width, 0, 0)
+                    .tickFormat("")
+                )
 
-                svg.append("g")
-                    .attr("class", "y axis")
-                    .call(yAxis)
-                    .append("text")
-                    .attr("transform", "rotate(-90)")
-                    .attr("y", 6)
-                    .attr("dy", ".71em")
-                    .style("text-anchor", "end")
-                    .text(yLabel);
+            svg.append("path")
+                .datum(sampledData)
+                .attr("class", "line")
+                .attr("d", line);
+        });
 
-                svg.append("g")
-                    .attr("class", "grid")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(make_x_axis()
-                        .tickSize(-height, 0, 0)
-                        .tickFormat("")
-                    )
+    };
+    return d3Func;
 
-                svg.append("g")
-                    .attr("class", "grid")
-                    .call(make_y_axis()
-                        .tickSize(-width, 0, 0)
-                        .tickFormat("")
-                    )
-
-                svg.append("path")
-                    .datum(sampledData)
-                    .attr("class", "line")
-                    .attr("d", line);
-            });
-
-        };
-        return d3Func;
-
-    });
+});
