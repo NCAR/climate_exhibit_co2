09db5c6052727636c48093c28a1443841555a7fb
Sharon Clark <sclark@ucar.edu>
2016-07-01 14:30:44 -0600

    put db close in finally block

--- data/harvest/harvest.py
+++ data/harvest/harvest.py
@@ -13,12 +13,7 @@ totalargs = len(sys.argv)
 total_processed = 0
 starttime = datetime.datetime.now()
 print 'Started at: %s' % starttime.strftime('%Y-%m-%d %H:%M:%S')
-
-# Open database connection
-db = MySQLdb.connect(db_config.host,db_config.username,db_config.password,db_config.database )
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
-        
+    
 # function to print usage
 def usage():
     print 'harvest.py -s <sitecode> -f <startfromend> -b <startfrombeginning> -e <endvalue>'
@@ -57,12 +52,12 @@ def main():
     filename = ''
     sitecode = ''
     
+    # retrieve arguments
     try:
         opts, args = getopt.getopt(argv, "hs:f:b:e:", ["sitecode=", "startfromend=", "startfrombeginning=", "endvalue="])
     except getopt.GetoptError:          
         usage()                      
         sys.exit(2)  
-    # retrieve arguments
     for opt, arg in opts:                
         if opt in ("-h", "--help"): 
             usage()
@@ -158,95 +153,102 @@ def main():
         sql_update = """UPDATE climate_co2_data2 SET co2_value=%s WHERE sitecode=%s AND timestamp_co2_recorded=%s"""
         values_insert = []
         values_update = []
-        for line in lastlines:
-            message_failure = ''
-            message_success = ''
-            parts = line.split()
-
-            if (len(parts) > 0):
-                year = parts[1]
-                month = parts[2].zfill(2)
-                day = parts[3].zfill(2)
-                hour = parts[4].zfill(2)
-                minute = parts[5].zfill(2)
-                sec = parts[6].zfill(2)
-                date_text = year+'-'+month+'-'+day+'T'+hour+':'+minute+':'+sec;
-                date = datetime.datetime.strptime(date_text, "%Y-%m-%dT%H:%M:%S")
-                timestamp = calendar.timegm(date.utctimetuple())
-                co2_value = parts[8]
-                    
-
-                # see if already exists in db
-                sql = "SELECT * FROM climate_co2_data2 WHERE sitecode='%s' AND timestamp_co2_recorded='%i'" % (sitecode , timestamp)
-                # Execute the SQL command
-                cursor.execute(sql)
-                # ensure no results before insert
-                numrows = cursor.rowcount
-                time_formatted = datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
-                if (numrows == 0):
-                    #db_transaction('insert', sitecode, str(co2_value), timestamp, 0)
-                    #sql = "INSERT INTO climate_co2_data2(sitecode, co2_value, timestamp_co2_recorded) VALUES('%s','%s','%i')" % (sitecode, co2_value, timestamp)
-                    # message_success = "Added %s - %s for %s to db.\r\n" % (time_formatted, co2_value, sitecode)
-                    # message_failure = "Could not commit %s - %s for %s to db.\r\n" % (time_formatted, co2_value, sitecode)  
-                    values_insert.append((sitecode, co2_value, timestamp));
-                    str_print.append("Attempting to add %s - %s for %s to db.\r\n" % (time_formatted, co2_value, sitecode))
-                else:
-                    #str_print.append("Value already exists on %s for %s.\r\n" % (datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S'), sitecode))
-                    # if more than 1 row - report as error
-                    if (numrows > 1):
-                        str_print.append("More than 1 value exists (%i: %i) in the db for %s and %s - you should investigate." % (numrows, int(timestamp), datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S'), sitecode))
-                    else:
-                        #check if values are different. If so, update
-                        data = cursor.fetchone()
-                        if (str(data[2]) != str(co2_value)):
-                            #db_transaction('update', sitecode, str(co2_value), timestamp, str(data[2]))
-                            #sql = "UPDATE climate_co2_data2 SET co2_value='%s' WHERE sitecode='%s' AND timestamp_co2_recorded='%i'" % (co2_value, sitecode, timestamp)
-                            #message_success = "Updated %s - %s from %s for %s to db.\r\n" % (time_formatted, co2_value, sitecode, data[2])
-                            #message_failure = "Could not update %s - %s from %s for %s to db.\r\n" % (time_formatted, co2_value, sitecode, data[2])
-                            values_update.append((co2_value, sitecode, timestamp));
-                            str_print.append("Attempting to update %s - %s from %s for %s to db.\r\n" % (time_formatted, co2_value, data[2], sitecode))
-        #inserts 
-        print " \n ".join(str_print)
-        if (len(values_insert) > 0):                                                                             
-            try:
-                # Execute the SQL command
-                #cursor.execute(sql)
-                cursor.executemany(sql_insert, values_insert)
-                # Commit your changes in the database
-                db.commit()
-                #print message_success
-                print 'Data committed'
-            except MySQLdb.Error, e:
-                try:
-                    print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-                except IndexError:
-                    print "MySQL Error: %s" % str(e)
-                # Rollback in case there is any error
-                db.rollback()
-                #print message_failure
-                print "Could not commit"    
-        #updates
-        if (len(values_update) > 0):                                                                             
-            try:
-                # Execute the SQL command
-                #cursor.execute(sql)
-                cursor.executemany(sql_update, values_update)
-                # Commit your changes in the database
-                db.commit()
-                #print message_success
-                print 'Data updated'
-            except MySQLdb.Error, e:
-                try:
-                    print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-                except IndexError:
-                    print "MySQL Error: %s" % str(e)
-                # Rollback in case there is any error
-                db.rollback()
-                #print message_failure
-                print "Could not update"        
-        # disconnect from server
-        db.close()
         
+        # Open database connection
+        db = MySQLdb.connect(db_config.host,db_config.username,db_config.password,db_config.database )
+        try:
+            # prepare a cursor object using cursor() method
+            cursor = db.cursor()
+
+            for line in lastlines:
+                message_failure = ''
+                message_success = ''
+                parts = line.split()
+
+                if (len(parts) > 0):
+                    year = parts[1]
+                    month = parts[2].zfill(2)
+                    day = parts[3].zfill(2)
+                    hour = parts[4].zfill(2)
+                    minute = parts[5].zfill(2)
+                    sec = parts[6].zfill(2)
+                    date_text = year+'-'+month+'-'+day+'T'+hour+':'+minute+':'+sec;
+                    date = datetime.datetime.strptime(date_text, "%Y-%m-%dT%H:%M:%S")
+                    timestamp = calendar.timegm(date.utctimetuple())
+                    co2_value = parts[8]
+
+
+                    # see if already exists in db
+                    sql = "SELECT * FROM climate_co2_data2 WHERE sitecode='%s' AND timestamp_co2_recorded='%i'" % (sitecode , timestamp)
+                    # Execute the SQL command
+                    cursor.execute(sql)
+                    # ensure no results before insert
+                    numrows = cursor.rowcount
+                    time_formatted = datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S')
+                    if (numrows == 0):
+                        #db_transaction('insert', sitecode, str(co2_value), timestamp, 0)
+                        #sql = "INSERT INTO climate_co2_data2(sitecode, co2_value, timestamp_co2_recorded) VALUES('%s','%s','%i')" % (sitecode, co2_value, timestamp)
+                        # message_success = "Added %s - %s for %s to db.\r\n" % (time_formatted, co2_value, sitecode)
+                        # message_failure = "Could not commit %s - %s for %s to db.\r\n" % (time_formatted, co2_value, sitecode)  
+                        values_insert.append((sitecode, co2_value, timestamp));
+                        str_print.append("Attempting to add %s - %s for %s to db.\r\n" % (time_formatted, co2_value, sitecode))
+                    else:
+                        #str_print.append("Value already exists on %s for %s.\r\n" % (datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S'), sitecode))
+                        # if more than 1 row - report as error
+                        if (numrows > 1):
+                            str_print.append("More than 1 value exists (%i: %i) in the db for %s and %s - you should investigate." % (numrows, int(timestamp), datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y-%m-%d %H:%M:%S'), sitecode))
+                        else:
+                            #check if values are different. If so, update
+                            data = cursor.fetchone()
+                            if (str(data[2]) != str(co2_value)):
+                                #db_transaction('update', sitecode, str(co2_value), timestamp, str(data[2]))
+                                #sql = "UPDATE climate_co2_data2 SET co2_value='%s' WHERE sitecode='%s' AND timestamp_co2_recorded='%i'" % (co2_value, sitecode, timestamp)
+                                #message_success = "Updated %s - %s from %s for %s to db.\r\n" % (time_formatted, co2_value, sitecode, data[2])
+                                #message_failure = "Could not update %s - %s from %s for %s to db.\r\n" % (time_formatted, co2_value, sitecode, data[2])
+                                values_update.append((co2_value, sitecode, timestamp));
+                                str_print.append("Attempting to update %s - %s from %s for %s to db.\r\n" % (time_formatted, co2_value, data[2], sitecode))
+            #inserts 
+            print " \n ".join(str_print)
+            if (len(values_insert) > 0):                                                                             
+                try:
+                    # Execute the SQL command
+                    #cursor.execute(sql)
+                    cursor.executemany(sql_insert, values_insert)
+                    # Commit your changes in the database
+                    db.commit()
+                    #print message_success
+                    print 'Data committed'
+                except MySQLdb.Error, e:
+                    try:
+                        print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                    except IndexError:
+                        print "MySQL Error: %s" % str(e)
+                    # Rollback in case there is any error
+                    db.rollback()
+                    #print message_failure
+                    print "Could not commit"    
+            #updates
+            if (len(values_update) > 0):                                                                             
+                try:
+                    # Execute the SQL command
+                    #cursor.execute(sql)
+                    cursor.executemany(sql_update, values_update)
+                    # Commit your changes in the database
+                    db.commit()
+                    #print message_success
+                    print 'Data updated'
+                except MySQLdb.Error, e:
+                    try:
+                        print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+                    except IndexError:
+                        print "MySQL Error: %s" % str(e)
+                    # Rollback in case there is any error
+                    db.rollback()
+                    #print message_failure
+                    print "Could not update"       
+        finally:
+            # disconnect from server
+            db.close()
         
     print 'Total items processed: '
     print total_processed
